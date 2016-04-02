4e4f3ec4c0c00d5477db2cabd946ed1743345fe6
Per Persson <>
2016-04-02 17:46:54 +0200

    Use the new commands

--- md2keynote.py
+++ md2keynote.py
@@ -10,9 +10,17 @@ if __name__ == '__main__':
     keynote = OSAScript(source)
     doc = keynote.newPresentation("White")
     # print keynote.themeMasters(doc)
-    slide = keynote.createSlide(doc, "Title & Subtitle", "Foo is the new Bar", "Per Persson")
-    slide = keynote.createSlide(doc, "Title, Bullets & Photo", "My items", "item 1\nitem 2")
+    slide = keynote.createSlide(doc, "Title & Subtitle")
+    keynote.addTitle(doc, "Foo is the new Bar", "Per Persson")
+    keynote.addBody(doc, "Per Persson")
+    
+    slide = keynote.createSlide(doc, "Title, Bullets & Photo")
+    keynote.addTitle(doc, "My items")
+    keynote.addBody(doc, "item 1\nitem 2")
     keynote.addImage(doc, "/Users/per/Desktop/bild.png")
     keynote.finalize(doc)
+    # keynote.deleteAllSlides(doc)
+    keynote.savePresentation(doc, "/Users/per/Documents/test88.key")
+
 
 
